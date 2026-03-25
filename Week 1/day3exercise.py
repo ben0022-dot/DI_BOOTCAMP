2bfa85375ec8313f0767ac1b35c8c0cfd382d779
BENEDICT <owinoben0022@gmail.com>
2026-03-25 12:27:08 +0300

    Add files via upload

--- Week 1/day3exercise.py
+++
@@ -1,7 +0,0 @@
-#Converting Lists into Dictionaries
-#keys = ['Ten', 'Twenty', 'Thirty']
-values = [10, 20, 30]
-#output {'Ten':10,'Twenty':20, 'Thirty':30}
-#Cinemax #2
-# Looping through dictionaries
-#
