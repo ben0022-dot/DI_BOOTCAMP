5f72c301f72d37ae5657cb744b4687887ba418dc
BENEDICT <owinoben0022@gmail.com>
2026-05-06 07:46:22 +0300

    Delete Week7/book.sql

--- Week7/book.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE book_db;
-USE book_db;
-
-CREATE TABLE books (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    "publishedYear" INTEGER NOT NULL
-);
